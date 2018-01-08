89b1d86a5b5315ef02f23fd6187573fa6929281c
Hamel Husain <hamel.husain@gmail.com>
2018-01-07 20:11:09 -0800

    update release

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 from setuptools import find_packages
+from setuptools.command.install import install
+import os
 
+class MyInstall(install):
+    "custom addition to download spacy files"
+    def run(self):
+        os.system("python -m spacy download en")
+        install.run(self)
 
 setup(
     name='kerasparalleltext',
     packages=find_packages(),
-    version='0.15',
+    version='0.18',
     description='Pre-processing text in parallel for Keras in python.',
     author='Hamel Husain',
     author_email='hamel.husain@gmail.com',
@@ -35,11 +42,12 @@ setup(
                   'pytest-cov',
                   'pandas'],
     },
-    download_url='https://github.com/hamelsmu/KerasParallelText/archive/0.15.tar.gz',
+    download_url='https://github.com/hamelsmu/KerasParallelText/archive/0.16.tar.gz',
     classifiers=[
         'Development Status :: 1 - Planning',
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+    ],
+    cmdclass={'install': MyInstall}
 )
