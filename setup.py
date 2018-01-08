b123d44d08a18aafa2e90393623388da4100a897
Hamel Husain <hamel.husain@gmail.com>
2018-01-07 19:10:58 -0800

    update release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(
     name='kerasparalleltext',
     packages=find_packages(),
-    version='0.12',
+    version='0.13',
     description='Pre-processing text in parallel for Keras in python.',
     author='Hamel Husain',
     author_email='hamel.husain@gmail.com',
@@ -33,7 +33,7 @@ setup(
                   'pytest-cov',
                   'pandas'],
     },
-    download_url='https://github.com/hamelsmu/KerasParallelText/archive/0.12.tar.gz',
+    download_url='https://github.com/hamelsmu/KerasParallelText/archive/0.13.tar.gz',
     classifiers=[
         'Development Status :: 1 - Planning',
         'Programming Language :: Python :: 3.6',
