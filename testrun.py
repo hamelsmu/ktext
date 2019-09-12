5b5a7536c7fec7bac027fd12e15dc718355e1bfa
Macide Celik <macide.35cc@gmail.com>
2019-09-13 01:08:40 +0300

    Delete testrun.py

--- testrun.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from ktext.preprocess import processor
-
-data_url = 'https://storage.googleapis.com/issue_label_bot/pre_processed_data/processed_part0000.csv'
-body = pd.read_csv(data_url).head(2000).text.tolist()
-issue_body_proc = processor(heuristic_pct_padding=.7, keep_n=5000)
-train_result = issue_body_proc.fit_transform(body)
